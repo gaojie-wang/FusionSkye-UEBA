e879216bcc3cba2bb0004e267a107831a97b4451
HaoranFei <hardyfeihaoran@126.com>
2018-06-14 01:30:50 -0700

    Cleanup code, also added tSNE support

--- CMB_anomaly_detection.py
+++ CMB_anomaly_detection.py
@@ -8,6 +8,7 @@ from keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 import csv 
 from ML_Package import *
+from operator import itemgetter
 
 __author__ = "Haoran Fei <hfei@andrew.cmu.edu>"
 
@@ -42,6 +43,76 @@ def filteraccount(parsed_csv, account_name):
 	else:
 		return False
 
+def preprocess(data):
+	data = np.array(data)
+	label_encoder = LabelEncoder()
+	label_encoder.fit(data.flatten())
+
+	integer_encoded = []
+
+	for i in range(len(data)):
+		integer_encoded.append(label_encoder.transform(data[i]))
+
+	##onehot_encoded = to_categorical(integer_encoded)
+
+	integer_encoded = np.array(integer_encoded)
+	return integer_encoded
+
+#Take the ith column of data (a list of lists) and return it as a list
+def take_column(data, i):
+	output = []
+	for j in range(len(data)):
+		output.append(data[j][i])
+	return output
+
+#Take the columns(specified in cols) of data (which is a list of lists)
+#Return a list of lists
+def take_columns(data, cols):
+	output = []
+	for row in data:
+		newrow = []
+		for j in cols:
+			newrow.append(row[j]) 
+		output.append(newrow)
+	return output
+
+
+#Perform dimensionality reduction
+def dim_reduce(data, viz_model, viz_dims):
+	if(viz_model == "PCA"):
+		data_process = Model_PCA(data, viz_dims)
+		data_process = data_process.T
+		return data_process
+	elif(viz_model == "tSNE"):
+		#To be Completed
+		#Dummy
+		return None
+
+#Visualize a dataset using viz_model for dimensionality reduciton
+#Reduce to viz_dims
+#Input: data_process is a 2-d array, but it has to be in Transpose!
+def visualize(data_process, viz_dims):
+	if viz_dims == 2:
+		x = data_process[0]
+		y = data_process[1]
+
+		#Use mathplotlib to visualize data
+		ax = plt.subplot(111)
+		ax.scatter(x, y)
+	
+	elif viz_dims == 3:
+		x = data_process[0]
+		y = data_process[1]
+		z = data_process[2]
+	
+		#Use mathplotlib to visualize data
+		ax = plt.subplot(111, projection='3d')
+		ax.scatter(x, y, z)
+
+	plt.show()
+
+
+#File Configs and Global Settings
 training_sample_size = 20000
 testing_sample_size = 500
 
@@ -56,16 +127,24 @@ counter = 0
 account_name = "5146C761F90BC0B17307EC91B47BE4AA"
 
 dims = [3,21,22]
+
 #Possible learning models:
 #"I": isolation forest
 #"D": DBSCAN
 #"L": Local Outlier Factor
-learning_model = "I"
-#PCA dimension
+learning_model = "D"
+
+#Visulization model
+#"PCA": PCA
+#"tSNE": tSNE
+viz_model = "PCA"
+
+#visualization dimension
 #2 or 3
-PCA_dims = 2
+viz_dims = 2
 
-
+#the number of highest-ranking anomalies to display
+select_num = 15
 
 
 #All dims of CMB data that are potentially useful
@@ -106,27 +185,6 @@ for row in reader:
 	if counter > training_sample_size+ testing_sample_size:
 		break
 
-def preprocess(data):
-	data = np.array(data)
-	label_encoder = LabelEncoder()
-	label_encoder.fit(data.flatten())
-
-	integer_encoded = []
-
-	for i in range(len(data)):
-		integer_encoded.append(label_encoder.transform(data[i]))
-
-	##onehot_encoded = to_categorical(integer_encoded)
-
-	integer_encoded = np.array(integer_encoded)
-	return integer_encoded
-
-#Take the ith column of data (a list of lists) and return it as a list
-def column(data, i):
-	output = []
-	for j in range(len(data)):
-		output.append(data[j][i])
-	return output
 
 
 itrain = preprocess(training_data)
@@ -145,6 +203,10 @@ elif learning_model == "L":
 	labels, scores = Model_LocalOutlierFactor(itrain)
 	print("Learning model selected is Local Outlier Factor")
 
+
+print(scores)
+
+
 anomaly_data = []
 anomaly_score = []
 #store the anomalous data
@@ -155,47 +217,31 @@ for i in range(len(training_data)):
 
 anomalies = zip(anomaly_data, anomaly_score)
 
-original = plt.subplot(111)
-x = column(anomaly_data, 1)
-y = column(anomaly_data, 2)
-original.scatter(x, y)
+#Take the 2nd and 3rd column of anomaly data
+#I.e. the columns without account name
+#and transform it to a transposed NumPy array
+anomaly_amounts = take_columns(anomaly_data, [1, 2])
+anomaly_amounts = np.array(anomaly_amounts)
+anomaly_amounts = anomaly_amounts.T
+visualize(anomaly_amounts, 2)
+
+select_anomalies = sorted(anomalies, key=itemgetter(1), reverse = True)
+select_anomalies = select_anomalies[:select_num]
 
 
 #print the anomalous data
-for data, score in anomalies:
+for data, score in select_anomalies:
 	print("Anomalous data: ")
 	print(data)
 	print(" Score: %.3f" % score + "\n")
 
-'''
-if PCA_dims == 2:
-	#PCA dimensionality reduction
-	itrain_process = Model_PCA(itrain, 2)
-	itrain_process = itrain_process.T
-	x = itrain_process[0]
-	y = itrain_process[1]
+#Visualization
+reduced = dim_reduce(itrain, viz_model, viz_dims)
+visualize(reduced, viz_dims)
 
-	#Use mathplotlib to visualize data
-	ax = plt.subplot(111)
-	ax.scatter(x, y)
-
-elif PCA_dims == 3:
-	#PCA dimensionality reduction
-	itrain_process = Model_PCA(itrain, 3)
-	itrain_process = itrain_process.T
-	x = itrain_process[0]
-	y = itrain_process[1]
-	z = itrain_process[2]
-	
-	#Use mathplotlib to visualize data
-	ax = plt.subplot(111, projection='3d')
-	ax.scatter(x, y, z)
-'''
-plt.show()
 
 #print(filterdimensions([1,2,3,4], [0,2]))
 #print(filterdimensions([1,2,3,4], [0,1,3]))
-
 
 
 
