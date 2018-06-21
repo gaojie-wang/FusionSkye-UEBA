b428b63ba5d7b97da4cfec57594fc383268273c0
HaoranFei <hardyfeihaoran@126.com>
2018-06-21 00:45:33 -0700

    Script for drawing transaction relationship completed

--- relation_graph.py
+++ relation_graph.py
@@ -1,16 +1,13 @@
 import matplotlib.pylab as plt
-from matplotlib.font_manager import FontProperties
-from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 import numpy as np
 import time
-from keras.utils import to_categorical
-from sklearn.preprocessing import LabelEncoder
 import csv 
 from ML_Package import *
 from operator import itemgetter
-import networkx
-
+import networkx as nx
+from random import randint
+from sets import Set 
 
 __author__ = "Haoran Fei <hfei@andrew.cmu.edu>"
 
@@ -29,47 +26,6 @@ def parseCSVstring(string):
 	list.append(string[start:len(string)])
 	return list
 
-
-#Dims is a list of integer, representing the 0-indexed indices of dimensions that we think
-#are meaningful and worth keeping
-def filterdimensions(parsed_csv, dims):
-	output = []
-	for i in dims:
-		output.append(parsed_csv[i])
-	return output
-
-#Returns whether the parsed_csv messasge belong to account_name
-#if account_name is empty string, then always return true
-def filteraccount(parsed_csv, account_name):
-	if account_name == "":
-		return True
-	elif parsed_csv[3] == account_name:
-		return True
-	else:
-		return False
-
-def preprocess(data):
-	data = np.array(data)
-	label_encoder = LabelEncoder()
-	label_encoder.fit(data.flatten())
-
-	integer_encoded = []
-
-	for i in range(len(data)):
-		integer_encoded.append(label_encoder.transform(data[i]))
-
-	##onehot_encoded = to_categorical(integer_encoded)
-
-	integer_encoded = np.array(integer_encoded)
-	return integer_encoded
-
-#Take the ith column of data (a list of lists) and return it as a list
-def take_column(data, i):
-	output = []
-	for j in range(len(data)):
-		output.append(data[j][i])
-	return output
-
 #Take the columns(specified in cols) of data (which is a list of lists)
 #Return a list of lists
 def take_columns(data, cols):
@@ -81,47 +37,50 @@ def take_columns(data, cols):
 		output.append(newrow)
 	return output
 
+#Input:
+#data is a list of lists
+#dim is the dimension of data to group by
+#return: a dict of list of lists
+def group_by(data, dim):
+	dict = {}
+	groups = Set()
+	for line in data:
+		if line[dim] not in groups:
+			groups.add(line[dim])
+			dict[line[dim]] = [line[:dim] + line[(dim+1):]]
+		else:
+			dict[line[dim]].append(line[:dim] + line[(dim+1):])
+	return dict  
 
-#Perform dimensionality reduction
-def dim_reduce(data, viz_model, viz_dims):
-	if(viz_model == "PCA"):
-		data_process = Model_PCA(data, viz_dims)
-		data_process = data_process.T
-		return data_process
-	elif(viz_model == "tSNE"):
-		#To be Completed
-		#Dummy
-		return None
+#Draw a labeled nx graph
+def draw_labeled_graph(G, num):	
+	pos=nx.get_node_attributes(G,'pos')
+	nx.draw(G,pos, with_labels=True)
+	labels = nx.get_edge_attributes(G,'weight')
+	nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+	#Figure size equal to the screen size of a 13.3inch laptop
+	plt.savefig("relation_graph_{}.png".format(num), dpi=300, fig_size=(11.4, 6.69))
+	plt.show()
+
 
 #Start calculating runtime
 start_time = time.time()
 
 
 #File Configs and Global Settings
-training_sample_size = 20000
-testing_sample_size = 500
+sample_size = 700000
 
 csvfile = open("CMB_1.csv")
 reader = csv.reader(csvfile, delimiter = " ")
 
-#Initial data
-#Parsed, but not filtered for accounts or dimensions
-raw_data = []
-
-training_data = []
-training_index = []
-testing_data = []
-
 #Largest account is 5146C761F90BC0B17307EC91B47BE4AA
 
+data = []
+graph_node = Set()
 
-#All dims of CMB data that are potentially useful
-#I have deleted all irrelevant dimensions
-#based on information provided by CMB officials
-#[3,6,12,13,14,15,16,17,20,21,22,23,24,28,29,30,56,57,59]
-
-
-
+#Graph data stored as adjacency list, implemented as dict of dicts
+graph_dict_by_transaction = {}
+graph_dict_by_amount = {}
 
 
 
@@ -138,93 +97,80 @@ for row in reader:
 	completeCSV = list1[:-1] 
 	completeCSV.append(list1[-1] + " " + list2[0])
 	completeCSV += list2[1:]
-	raw_data.append(completeCSV)
+	data.append(completeCSV)
 
-	#filter by account, if applicable
-	if filteraccount(completeCSV, account_name):
+	#add all accounts to graph_node
+	account = completeCSV[3]
+	if account not in graph_node:
+		graph_node.add(account)
 
-		#Filter out the dimensions that we are interested in 
-		paramlist = filterdimensions(completeCSV, dims)
-
-		#Add to training or testing data
-		if(counter < training_sample_size):
-			training_data.append(paramlist)
-			training_index.append(counter)
-		else:
-			testing_data.append(paramlist)
-	
-	#Increment total counter
 	counter += 1
-
-	#Enough samples, stop reading from csv file
-	if counter > training_sample_size+ testing_sample_size:
+	if counter > sample_size:
 		break
 
+#Extracting 4 dimensions: account number, group number, transaction direction and transaction amount
+data = take_columns(data, [3, 12, 20, 21])
 
-
-itrain = preprocess(training_data)
-itest = preprocess(testing_data)
-
-
-#Use the selected model to do unsupervised ML
-#And discover anomalies
-if learning_model == "I":
-	labels, scores = Model_IsolationForest(itrain)
-	print("Learning model selected is Isolation Forest")
-elif learning_model == "L":
-	labels, scores = Model_LocalOutlierFactor(itrain)
-	print("Learning model selected is Local Outlier Factor")
-
-
-anomaly_data = []
-anomaly_index = []
-anomaly_score = []
-#store the anomalous data
-for i in range(len(training_data)):
-	if labels[i] == -1:
-		anomaly_data.append(training_data[i])
-		anomaly_index.append(training_index[i])
-		anomaly_score.append(scores[i])
-
-
-#Visualize all data
-all_amounts = take_columns(training_data, [1, 2])
-all_amounts = np.array(all_amounts)
-all_amounts = all_amounts.T
-visualize(all_amounts, 2)
+grouped_data = group_by(data, 1)
 
 
 
-#Take the 2nd and 3rd column of anomaly data
-#I.e. the columns without account name
-#and transform it to a transposed NumPy array
-anomaly_amounts = take_columns(anomaly_data, [1, 2])
-anomaly_amounts = np.array(anomaly_amounts)
-anomaly_amounts = anomaly_amounts.T
-visualize(anomaly_amounts, 2)
+#initialize two graph_dicts
+for node in graph_node:
+	graph_dict_by_transaction[node] = {}
+	graph_dict_by_amount[node] = {}
 
 
-anomalies = zip(anomaly_data, anomaly_index, anomaly_score)
+for node in graph_node:
+	for node2 in graph_node:
+		graph_dict_by_transaction[node][node2] = 0
+		graph_dict_by_amount[node][node2] = 0
 
-select_anomalies = sorted(anomalies, key=itemgetter(2), reverse = True)
-select_anomalies = select_anomalies[:select_num]
+for group, vectors in grouped_data.iteritems():
+	L = len(vectors)
+	for i in range(L):
+		for j in range(L):
+			account1 = vectors[i][0]
+			account2 = vectors[j][0]
+			#Different account, different transaction direction
+			if account1 != account2 and vectors[i][1] != vectors[j][1]:
+				#increment weight by 1 since weight represents number of transactions
+				graph_dict_by_transaction[account1][account2] += 1
+
+				#increment weight by absolute value of transaction amount
+				graph_dict_by_amount[account1][account2] += abs(float(vectors[i][2]))
 
 
-#print the anomalous data
-for data, index, score in select_anomalies:
-	print("Anomalous data: ")
-	print(data)
-	print("Event number: ")
-	print(raw_data[index][0])
-	print("Score: %.3f" % score + "\n")
 
-#Visualization
-reduced = dim_reduce(itrain, viz_model, viz_dims)
-visualize(reduced, viz_dims)
+#Start creating networkx graph
+#G1: only counts number of transaction 
+#G2: counts transaction amounts
+G1 = nx.Graph()
+G2 = nx.Graph()
+
+for node in graph_node:
+	rand_pos = (randint(0, len(graph_node)), randint(0, len(graph_node)))
+	G1.add_node(node, pos=rand_pos)
+	G2.add_node(node, pos=rand_pos)
+
+#Build the first graph
+for node1, ndict in graph_dict_by_transaction.iteritems():
+	for node2, w in ndict.iteritems():
+		#If weight is none-zero
+		if w != 0:
+			G1.add_edge(node1, node2, weight=w)
+
+#Build the second graph
+for node1, ndict in graph_dict_by_amount.iteritems():
+	for node2, w in ndict.iteritems():
+		#if weight is non-zero
+		if abs(w) >= 0.00000001:
+			G2.add_edge(node1, node2, weight=w)
 
 
-#Display runtime
-print("--- %s seconds ---" % (time.time() - start_time))
+#Draw both labeled graphs
+draw_labeled_graph(G1, 1)
+draw_labeled_graph(G2, 2)
 
-#print(filterdimensions([1,2,3,4], [0,2]))
-#print(filterdimensions([1,2,3,4], [0,1,3]))
+
+
